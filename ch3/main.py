b46479e3272f3257879468d4a3388178ce4b3aed
darkaccount <37471870@qq.com>
2017-07-14 18:29:42 +0800

    use session to GET

--- ch3/main.py
+++ ch3/main.py
@@ -52,8 +52,24 @@ def timeout_request():
         print (response.status_code)
 
 
+def hard_requests():
+    from requests import Request, Session
+    s = Session()
+    headers = {'User-Agent': 'fake1.3.4'}
+    req = Request('Get', build_uri('user/emails'), auth=('imoocdemo', 'imoocdemo123'), headers=headers)
+    prepped = req.prepare()
+    print (prepped.body)
+    print (prepped.headers)
+
+    resp = s.send(prepped, timeout=5)
+    print (resp.status_code)
+    print (resp.request.headers)
+    print (resp.text)
+
+
 if __name__ == '__main__':
 #    request_method()
 #    params_request()
 #    json_request()
-    timeout_request()
+#    timeout_request()
+    hard_requests()
